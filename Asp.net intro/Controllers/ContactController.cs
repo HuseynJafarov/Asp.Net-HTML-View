c25a93082f5d02e2ad6c4209c277d70b194d7f27
HuseynJafarov <huseynnj@code.edu.az>
2022-10-02 17:46:48 +0400

    ClassWork

---
+++ Asp.net intro/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_intro.Controllers
+{
+    public class ContactController : Controller
+    {
+        public string Index()
+        {
+            return "Contact Page";
+        }
+    }
+}
---
+++ Asp.net intro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(int id, string name)
+        {
+            return "Home Page" +" "+ id + "-" + name;
+        }
+
+        public IActionResult FullName()
+        {
+            return Json(new { name = "Huseyn", Surname = "Jafarov" });
+            //Content()
+        }
+
+        public IActionResult Html()
+        {
+            return View();
+        }
+
+        public string FullData(string name, string surname,int age)
+        {
+            return $"Name--{name}: Surname--{surname}: Age--{age}";
+        } 
+    }
+}
